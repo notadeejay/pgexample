e7f2b91c5367b1e735ff428ae7b74d75f4491db6
Amy <amy@lighthouselabs.ca>
2017-08-15 15:54:44 +0000

    Added foreign key to Milestones

--- migrations/20170815150303_create_milestones.js
+++ migrations/20170815150303_create_milestones.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('milestones', function(table){
-      table.increments('id').primary();
+      milestones.increments('id').primary();
       table.string('description');
       table.date('date_acheived');
     })
