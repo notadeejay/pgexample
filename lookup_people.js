c2e9a95676fadc1f8bf76bbb0938f05f6de2fbd1
Amy <amy@lighthouselabs.ca>
2017-08-15 13:34:06 +0000

    Added moment to format date

--- lookup_people.js
+++ lookup_people.js
@@ -1,5 +1,6 @@
 const pg = require("pg");
-const settings = require("./settings"); // settings.json
+const settings = require("./settings");
+const moment = require("moment")
 const args = process.argv[2];
 
 const client = new pg.Client({
@@ -11,18 +12,6 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
-function formatDate (date) {
-  let dateObj = new Date(date);
-  let month = dateObj.getUTCMonth() + 1;
-  let day = dateObj.getUTCDate();
-  let year = dateObj.getUTCFullYear();
-
-  if (month < 10) {
-    return year + "-0" + month + "-" + day;
-  } else {
-    return year + "-" + month + "-" + day;
-  }
-}
 
 client.connect((err) => {
   if (err) {
@@ -34,7 +23,7 @@ client.connect((err) => {
     }
 
 
-    console.log(`- 1: ${res.rows[0].first_name} ${res.rows[0].last_name}, born ${formatDate(res.rows[0].birthdate)}`);
+    console.log(`- 1: ${res.rows[0].first_name} ${res.rows[0].last_name}, born ${moment(res.rows[0].birthdate).format("YYYY-MM-D")}`);
     client.end();
 
   });
